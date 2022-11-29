2c81b0b9764f90b9a61984a5638b49dd313aaafa
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-11-29 15:22:32 -0300

    Curso 01 · Aula 02 · Atividade 05 · Compile e rode seu primeiro programa Java

---
+++ curso-01-java-parte-01-primeiros-passos/src/HelloWorld.java
@@ -0,0 +1,8 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, Mundo!");   
+  }
+}

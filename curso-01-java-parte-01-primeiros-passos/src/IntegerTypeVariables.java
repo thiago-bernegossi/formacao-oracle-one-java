5f5d7ddb147cdc9a697a1f894413640a3c636a31
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-11-29 15:39:10 -0300

    Curso 01 · Aula 04 · Atividade 01 · Tipo inteiro, int

---
+++ curso-01-java-parte-01-primeiros-passos/src/IntegerTypeVariables.java
@@ -0,0 +1,12 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+public class IntegerTypeVariables {
+	
+	public static void main(String[] args) {		
+		int userAge = 50;
+		System.out.println("A-) A idade do usuário Donatello é " + (userAge - 25) + " anos;");		
+		System.out.println("B-) A idade do usuário Leonardo é " + (userAge / 2) + " anos;");		
+		System.out.println("C-) A idade do usuário Michelangelo é " + ((userAge * 2) / 4) + " anos;");		
+		System.out.print("D-) A idade do usuário Raphael é " + ((userAge + 50) - 75) + " anos.");
+	}
+}

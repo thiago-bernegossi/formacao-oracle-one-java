4d14510813d38d867c13d453eda3ff7ecdf28a54
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-11-30 14:30:52 -0300

    Curso 01 · Aula 07 · Atividade 01 · Laço com While

---
+++ curso-01-java-parte-01-primeiros-passos/src/TheWhileLoop.java
@@ -0,0 +1,13 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+public class TheWhileLoop {
+
+	public static void main(String[] args) {
+		int value = 0;
+		
+		while (value <= 50) {
+			System.out.println("O valor é " + value + ".");
+			value++;
+		}
+	}
+}

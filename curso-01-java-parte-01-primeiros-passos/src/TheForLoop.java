504278917ea6b3ff6e3183aaab6cf5362f4b896a
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-11-30 14:46:42 -0300

    Curso 01 · Aula 07 · Atividade 06 · Laço com For

---
+++ curso-01-java-parte-01-primeiros-passos/src/TheForLoop.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+public class TheForLoop {
+
+	public static void main(String[] args) {
+  
+		for (int value = 0; value <= 50; value++) {
+			System.out.println("O valor é " + value + ".");
+		}
+	}
+}

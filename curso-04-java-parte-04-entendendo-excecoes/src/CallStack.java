93996c176b3d75cbbd47e6929642c21214e109bd
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-12-13 17:17:14 -0300

    Curso 04 · Aula 01 · Atividade 05 · Depuração

---
+++ curso-04-java-parte-04-entendendo-excecoes/src/CallStack.java
@@ -0,0 +1,30 @@
+//Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+public class CallStack {
+
+	public static void main(String[] args) {
+		System.out.println("Início da execução do método main.");
+
+		firstMethod();
+
+		System.out.println("Final da execução do método main.");
+	}
+
+	private static void firstMethod() {
+		System.out.println("Início da execução do método firstMethod.");
+
+		secondMethod();
+
+		System.out.println("Final da execução do método firstMethod.");
+	}
+
+	private static void secondMethod() {
+		System.out.println("Início da execução do método secondMethod.\n");
+
+		for (int counter = 0; counter <= 10; counter++) {
+			System.out.println(counter);
+		}
+
+		System.out.println("\nFinal da execução do método secondMethod.");
+	}
+}

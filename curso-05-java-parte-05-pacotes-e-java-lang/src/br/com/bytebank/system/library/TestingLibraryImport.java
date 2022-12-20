ccc800d8216edcee96d8df59e6a2c709441c2271
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-12-20 15:57:40 -0300

    Curso 05 · Aula 03 · Atividade 05 · Criando uma biblioteca com JAR

---
+++ curso-05-java-parte-05-pacotes-e-java-lang/src/br/com/bytebank/system/library/TestingLibraryImport.java
@@ -0,0 +1,40 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+package br.com.bytebank.system.library;
+
+import br.com.bytebank.system.model.CheckingAccountWithUncheckedException;
+
+public class TestingLibraryImport {
+
+	public static void main(String[] args) {		
+		System.out.println("***** Olá! Seja muito bem-vindo(a) ao ByteBank! *****\n");
+		
+		CheckingAccountWithUncheckedException firstAccount = new CheckingAccountWithUncheckedException("Primeira Conta", 654321, 123456789);		
+		
+		System.out.println("O apelido da conta é " + firstAccount.getAccountNickname() + ".");
+		System.out.println("A agência da " + firstAccount.getAccountNickname() + " é " + firstAccount.getAccountAgency() + ".");	
+		System.out.println("O número da " + firstAccount.getAccountNickname() + " é " + firstAccount.getAccountNumber() + ".");	
+		System.out.println("O saldo atual da " + firstAccount.getAccountNickname() + " é R$ " + firstAccount.getAccountBalance() + ".");
+		
+		System.out.println();
+		
+		CheckingAccountWithUncheckedException secondAccount = new CheckingAccountWithUncheckedException("Segunda Conta", 654321, 234567891);	
+		
+		System.out.println("O apelido da conta é " + secondAccount.getAccountNickname() + ".");
+		System.out.println("A agência da " + secondAccount.getAccountNickname() + " é " + secondAccount.getAccountAgency() + ".");	
+		System.out.println("O número da " + secondAccount.getAccountNickname() + " é " + secondAccount.getAccountNumber() + ".");	
+		System.out.println("O saldo atual da " + secondAccount.getAccountNickname() + " é R$ " + secondAccount.getAccountBalance() + ".");
+
+		firstAccount.depositValue(100);
+		firstAccount.transferValue(50, secondAccount);
+		
+		System.out.println();
+		
+		System.out.println("O saldo atual da " + firstAccount.getAccountNickname() + " é R$ " + firstAccount.getAccountBalance() + ".");
+		System.out.println("O saldo atual da " + secondAccount.getAccountNickname() + " é R$ " + secondAccount.getAccountBalance() + ".");
+		
+		System.out.println("\nObs.: A quantidade de contas abertas no ByteBank é " + CheckingAccountWithUncheckedException.getAmountOfBankAccounts() + ".");
+		
+		System.out.println("\n***** Obrigado(a) por utilizar nossos serviços. *****");
+	}
+}

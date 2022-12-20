9901cb971d3ac1b883a63da568208b5e25d07b31
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-12-20 17:07:51 -0300

    Curso 05 · Aula 04 · Atividade 01 · Conhecendo java.lang

---
+++ curso-05-java-parte-05-pacotes-e-java-lang/src/br/com/bytebank/system/test/TestingStringClass.java
@@ -0,0 +1,11 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+package br.com.bytebank.system.test;
+
+import java.lang.*;
+
+public class TestingStringClass {
+
+	public static void main(String[] args) {
+	}
+}

a7b51e699fdedb8ee8f3e2e3840ad9f44f301dae
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-12-20 17:21:53 -0300

    Curso 05 · Aula 04 · Atividade 04 · String e a imutabilidade

--- curso-05-java-parte-05-pacotes-e-java-lang/src/br/com/bytebank/system/test/TestingStringClass.java
+++ curso-05-java-parte-05-pacotes-e-java-lang/src/br/com/bytebank/system/test/TestingStringClass.java
@@ -2,10 +2,19 @@
 
 package br.com.bytebank.system.test;
 
-import java.lang.*;
-
 public class TestingStringClass {
 
 	public static void main(String[] args) {
+		String firstVersion = "ByteBank";		
+		String firstMessage = ("***** Olá! Seja muito bem-vindo(a) ao " + firstVersion + "! *****\n");
+		System.out.println(firstMessage);
+		
+		String secondVersion = firstVersion.toLowerCase();
+		String secondMessage = ("***** Olá! Seja muito bem-vindo(a) ao " + secondVersion + "! *****\n");
+		System.out.println(secondMessage);
+		
+		String thirdVersion = secondVersion.replace("b", "B");
+		String thirdMessage = ("***** Olá! Seja muito bem-vindo(a) ao " + thirdVersion + "! *****\n");
+		System.out.println(thirdMessage);
 	}
 }

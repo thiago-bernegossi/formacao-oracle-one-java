25c748b45d7fd49da414112a46b4bb2eef7c7a42
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-12-22 15:50:31 -0300

    Curso 06 · Aula 02 · Atividade 08 · Entendendo o array String args

---
+++ curso-06-java-parte-06-conhecendo-o-java-util/src/br/com/bytebank/system/test/TestingArrayWithString.java
@@ -0,0 +1,16 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação Java™.
+
+package br.com.bytebank.system.test;
+
+public class TestingArrayWithString {
+
+	public static void main(String[] args) {
+		// System.out.println("***** Olá! Seja muito bem-vindo(a) ao ByteBank! *****\n");
+		
+		for (int counter = 0; counter < args.length; counter++) {
+			System.out.println(args[counter]);
+		}
+
+		// System.out.println("\n***** Obrigado(a) por utilizar nossos serviços. *****");
+	}
+}

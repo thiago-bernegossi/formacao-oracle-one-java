35102c837b85812e999d72d782a4ecfeef76a6db
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-12-27 18:12:40 -0300

    Curso 07 · Aula 01 · Atividade 04 · InputStream e Reader

--- curso-07-java-parte-07-trabalhando-com-java-io/src/br/com/bytebank/system/test/io/TestingInputStreamWithFile.java
+++ curso-07-java-parte-07-trabalhando-com-java-io/src/br/com/bytebank/system/test/io/TestingInputStreamWithFile.java
@@ -5,14 +5,17 @@ package br.com.bytebank.system.test.io;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TestingInputStreamWithFile {
 
 	public static void main(String[] args) throws IOException {
-		FileInputStream fileInputStream = new FileInputStream("lorem.txt");
-		InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		// Processo de Fluxo de Entrada de Dados com Arquivo.
+		InputStream fileInputStream = new FileInputStream("lorem.txt"); // Início do Padrão de Projeto Decorator.
+		Reader inputStreamReader = new InputStreamReader(fileInputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // Final do Padrão de Projeto Decorator.
 		
 		String firstLine = bufferedReader.readLine();
 		System.out.println("O conteúdo da primeira linha do texto é: \n" + firstLine);

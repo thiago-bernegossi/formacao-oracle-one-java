5866be4b7cc537b3201d14d0e78905834fddf8b6
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2023-01-07 17:35:45 -0300

    Curso 09 · Aula 05 · Atividade 01 · Mais Streams, Collectors e APIs

--- curso-09-java-8-tire-proveito-dos-novos-recursos-da-linguagem/src/br/com/bytebank/learn/test/TestingTheCourseClass.java
+++ curso-09-java-8-tire-proveito-dos-novos-recursos-da-linguagem/src/br/com/bytebank/learn/test/TestingTheCourseClass.java
@@ -5,6 +5,8 @@ package br.com.bytebank.learn.test;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 import br.com.bytebank.learn.model.Course;
 
@@ -20,14 +22,23 @@ public class TestingTheCourseClass {
 		
 		courses.sort(Comparator.comparingInt(Course::getNumberOfRegistrations));
 		
-		System.out.println(courses);
-		
-		System.out.println();
-		
-		int numberOfRegistrations = courses.stream()
+		OptionalDouble average = courses.stream()
 				.filter(course -> course.getNumberOfRegistrations() >= 100)
-				.mapToInt(Course::getNumberOfRegistrations).sum();
+				.mapToInt(Course::getNumberOfRegistrations)
+				.average();
 		
-		System.out.println("A-) O total de alunos matriculados em cursos com mais de 100 matrículas é: " + numberOfRegistrations + ".\n");
+		System.out.println("A-) A média de matrículas em cursos com 100 ou mais matrículas é: " + average.getAsDouble() + ".\n");
+		
+		courses.stream()
+				.filter(course -> course.getNumberOfRegistrations() >= 100)
+				.findAny()
+				.ifPresent(course -> System.out.println("B-) Um dos cursos com 100 ou mais matrículas é: " + course.getTitle() + ".\n"));
+	
+		courses.stream()
+				.filter(course -> course.getNumberOfRegistrations() >= 100)
+				.collect(Collectors.toMap(
+						course -> course.getTitle(),
+						course -> course.getNumberOfRegistrations()))
+				.forEach((title, numberOfRegistrations) -> System.out.println("C-) Um dos cursos com 100 ou mais matrículas é: \n[Curso: " + title + "; " + "Matrículas: " + numberOfRegistrations + ".]\n")); 
 	}
 }
